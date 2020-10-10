e6d33d3cfc8ebc7eac0741e5c3a416bf6293b779
Jamie Coakley <jamiemcoakley@gmail.com>
2020-10-10 10:12:51 -0400

    deleted duplicate files and folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import Home from './Home/Home'
 import './App.css';
+import {createMuiTheme} from '@material-ui/core/styles'
+import lime from '@material-ui/core/colors/lime'
+
+const theme = createMuiTheme({
+  status: {
+    danger: 
+  }
+  palette: {
+    primary: {
+      main: '#29b6f6'
+
+    },
+    secondary: lime,
+  }
+})
 
 function App() {
 
