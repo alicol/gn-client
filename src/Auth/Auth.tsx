24519025315bd9c688fd2cb875f2db76289d46ef
Adam Martin <adam.d.martin1986@gmail.com>
2020-10-13 20:32:02 -0400

    continued to implement admin

--- src/Auth/Auth.tsx
+++ src/Auth/Auth.tsx
@@ -7,7 +7,7 @@ import {
 } from 'react-router-dom';
 import UserLogin from './UserLogin';
 import UserSignup from './UserSignup';
-import AdminLogin from './AdminLogin';
+
 
 export interface AuthProps {
 
@@ -22,12 +22,10 @@ const Auth: React.SFC<AuthProps> = () => {
                 <div>
                     <button><Link to="/UserLogin">Login!</Link></button>
                     <button><Link to="/UserSignup">Sign Up!</Link></button>
-                    <button><Link to="/AdminLogin">Admin Login</Link></button>
                     <hr />
                     <Switch>
                         <Route exact path='/UserLogin' component={UserLogin} />
                         <Route exact path='/UserSignup' component={UserSignup}/>
-                        <Route exact path='/AdminLogin' component={AdminLogin}/>
                     </Switch>
                 </div>
             </Router>
