63b23011fa13161ab274531dabfd2b7e3caa07f2
Adam Martin <adam.d.martin1986@gmail.com>
2020-10-18 21:13:32 -0400

    added missing code from develop branch

--- src/Admin/UserTable.tsx
+++ src/Admin/UserTable.tsx
@@ -1,3 +1,4 @@
+import { findAllByPlaceholderText } from '@testing-library/react';
 import React, { useState, useEffect } from 'react';
 
 
@@ -10,15 +11,17 @@ export interface UserTableProps {
 }
 
 export interface UserTableState {
-  
+  updatedUserName: string,
   users: any,
+  showUpdateUser: boolean,
+  editUserId: number | null
 
 }
 
 class UserTable extends React.Component<UserTableProps, UserTableState> {
   constructor(props: UserTableProps) {
     super(props);
-    this.state = {users: []};
+    this.state = {users: [], updatedUserName: "", showUpdateUser: false, editUserId: null};
   }
 
   URL = "http://localhost:3000";
@@ -51,6 +54,17 @@ class UserTable extends React.Component<UserTableProps, UserTableState> {
         const userName = user.userName;
         const permission = user.permission;
 
+        const changeUserName = () => {
+          if (this.state.showUpdateUser == true && this.state.editUserId == id){
+            return(
+              <div>
+                <input placeholder={userName} type="text" onChange={(e) => this.setState({updatedUserName: e.target.value})}/>
+                <button onClick={(e) => this.setState({editUserId: null})}>Cancel</button>
+                <button type="submit" onClick={(e) => {this.editUser(id); this.setState({showUpdateUser: false})}}> Submit Change</button>
+              </div>
+            )
+          }
+        }
 
        
         return (
@@ -58,9 +72,11 @@ class UserTable extends React.Component<UserTableProps, UserTableState> {
             <h3>{`User #${id}`}</h3>
             <ul>
               <li>{`UserName: ${userName}`}</li>
+              {changeUserName()}
               <li>{`Permission: ${permission}`}</li>
             </ul>
             <button onClick={(e) => this.deleteUser(id)}>Delete User</button>
+            <button onClick={(e) => {this.setState({showUpdateUser: true}); this.setState({editUserId: id})}}>Edit UserName</button>
           </div>
         )
       })
@@ -85,6 +101,28 @@ deleteUser = (userId:number) => {
     })
 }
 
+editUser = (userId:number) => {
+  fetch(`${this.URL}/user/edit/${userId}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      user: {
+       userName: this.state.updatedUserName,
+     
+      },
+  }),
+    headers: new Headers({
+      "Content-Type": "application/json",
+      Authorization: this.props.token,
+    }),
+  })
+    .then((res) => res.json())
+    .then((logData) => {
+      alert("This user has been edited.")
+      console.log(logData);
+      this.getUsers();
+
+    })
+}
 
 
 
