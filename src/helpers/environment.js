5dc813a16fd535b838e146b0ddf344ac91f2b3a6
alicol <alicol747@gmail.com>
2020-10-24 12:11:00 -0400

    changed environment

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname){
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:<3000>';
         break;
-    case 'https://ss-gn-server.herokuapp.com':
+    case 'https://ss-gn-client.herokuapp.com':
         APIURL = 'https://ss-gn-server.herokuapp.com'
 }
 
